569c780a9f01928ed06997fecb673ab618ae34fe
alantang <alanchao@163.com>
2025-06-10 12:22:36 +0800

    Update IPTV搜索引擎采集.py

--- py/IPTV搜索引擎采集.py
+++ py/IPTV搜索引擎采集.py
@@ -40,7 +40,7 @@ def via_tonking(url):
         # 提取 IP 地址部分，去除协议
         ip_address = url.split("//")[-1]
         response = requests.get(
-            url=f'https://tonkiang.us/hoteliptv.php?page=1&iphone1=北京市&code=',
+            url=f'https://tonkiang.us/hoteliptv.php?page=1&iphone1=广州市&code=',
             headers=headers,
             timeout=10
         )
